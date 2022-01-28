9343470bb4661f57b0888e5dcecd773adfdd3b68
Haris Volos <hvolos@Hariss-MacBook-Air.local>
2022-01-28 10:14:44 +0200

    Add common startup script

---
+++ startup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Setup password-less ssh between nodes
+for user in $USERS; do
+    if [ "$user" = "root" ]; then
+        ssh_dir=/root/.ssh
+    else
+        ssh_dir=/users/$user/.ssh
+    fi
+    /usr/bin/geni-get key > $ssh_dir/id_rsa
+    chmod 600 $ssh_dir/id_rsa
+    chown $user: $ssh_dir/id_rsa
+    ssh-keygen -y -f $ssh_dir/id_rsa > $ssh_dir/id_rsa.pub
+    cat $ssh_dir/id_rsa.pub >> $ssh_dir/authorized_keys
+    chmod 644 $ssh_dir/authorized_keys
+    cat >>$ssh_dir/config <<EOL
+    Host *
+         StrictHostKeyChecking no
+EOL
+    chmod 644 $ssh_dir/config
+done
+
+# Change user login shell to Bash
+for user in `ls /users`; do
+    chsh -s `which bash` $user
+done
